42a25abd8683b99fccd08efb76b9cbc51484634d
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2013-05-31 13:00:55 +0200

    updated ufw module

--- modules/firewall/manifests/ufw.pp
+++ modules/firewall/manifests/ufw.pp
@@ -1,12 +1,33 @@
-class firewall::ufw {
+class firewall::ufw($status) {
 
 	$app_config_dir = '/etc/ufw/applications.d'
 
 	package { 'ufw':
 		ensure => present,
 	}
-	exec { '/usr/sbin/ufw --force enable':
-		unless    => "/usr/sbin/ufw status | /bin/grep \"Status: active\"",
+
+	if $status == 'active' {
+
+		exec { '/usr/sbin/ufw --force enable':
+			unless => "/usr/sbin/ufw status | /bin/grep \"Status: active\"",
+		}
+
+		# make rules are set before activating the firewall
+		Firewall::Ufw::App<| |> -> Exec['/usr/sbin/ufw --force enable']
+
+	} elsif $status == 'inactive' {
+
+		exec { '/usr/sbin/ufw --force disable':
+			unless => "/usr/sbin/ufw status | /bin/grep \"Status: inactive\"",
+		}
+
+		# disable the firewall before changing rules
+		Exec['/usr/sbin/ufw --force disable'] -> Firewall::Ufw::App <| |>
+
+	} else {
+
+		err("invalid status for firewall::ufw (status: $status)")
+
 	}
 
 }
--- modules/firewall/manifests/ufw/app.pp
+++ modules/firewall/manifests/ufw/app.pp
@@ -1,21 +1,51 @@
-define firewall::ufw::app ($state, $from = "any") {
+define firewall::ufw::app ($state, $from = "any", $ensure = present) {
 
 	include firewall::ufw
 
-	if $state == 'allow' {
-		exec { "/usr/sbin/ufw allow from $from to any app '$name'":
-			logoutput => on_failure,
-			unless    => "/usr/sbin/ufw status | /bin/grep -i \"$name *ALLOW *$from\"",
-			require   => Package['ufw'],
+	# if status is inactive we cannot query for current rules
+
+	if $::firewall::ufw::status == 'active' {
+
+		if $state == 'allow' {
+			if $ensure == present {
+
+				exec { "/usr/sbin/ufw allow from $from to any app '$name'":
+					unless  => "/usr/sbin/ufw status | /bin/grep -i \"$name *ALLOW *$from\"",
+					require => Package['ufw'],
+				}
+
+			}
+			else {
+
+				exec { "/usr/sbin/ufw delete allow from $from to any app '$name'":
+					onlyif  => "/usr/sbin/ufw status | /bin/grep -i \"$name *ALLOW *$from\"",
+					require => Package['ufw'],
+				}
+
+			}
 		}
-	} elsif $state == 'deny' {
-		exec { "/usr/sbin/ufw deny from $from to any app '$name'":
-			logoutput => on_failure,
-			unless    => "/usr/sbin/ufw status | /bin/grep -i \"$name *DENY *$from\"",
-			require   => Package['ufw'],
+		elsif $state == 'deny' {
+			if $ensure == present {
+
+				exec { "/usr/sbin/ufw deny from $from to any app '$name' > /dev/null":
+					unless  => "/usr/sbin/ufw status | /bin/grep -i \"$name *DENY *$from\"",
+					require => Package['ufw'],
+				}
+
+			}
+			else {
+
+				exec { "/usr/sbin/ufw delete deny from $from to any app '$name' > /dev/null":
+					onlyif   => "/usr/sbin/ufw status | /bin/grep -i \"$name *DENY *$from\"",
+					require => Package['ufw'],
+				}
+
+			}
 		}
-	} else {
-		err("invalid state for firewall::ufw::app (state: $state)")
+		else {
+			err("invalid state for firewall::ufw::app (state: $state)")
+		}
+
 	}
 
 }
